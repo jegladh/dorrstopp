49a16a731ef844392bd322bb509c124e26412a15
jegladh <jesgla-2@student.ltu.se>
2021-09-06 14:53:28 +0200

    glömt stage (#2)
    * First
    
    * Bakgrund, textfärg, visa examen, kontaktsida
    
    * Bakgrund, textfärg, visa examen, kontaktsida

---
+++ dorrstopp/src/app/components/ds-kontakt/ds-kontakt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ds-kontakt',
+  templateUrl: './ds-kontakt.component.html',
+  styleUrls: ['./ds-kontakt.component.css']
+})
+export class DsKontaktComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
