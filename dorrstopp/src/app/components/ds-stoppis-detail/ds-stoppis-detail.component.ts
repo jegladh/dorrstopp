d5a3e103f688696d3d806537f072a027a84ef24f
niklas <niklas.brynolf@gmail.com>
2021-09-08 23:51:46 +0200

    service

--- dorrstopp/src/app/components/ds-stoppis-detail/ds-stoppis-detail.component.ts
+++ dorrstopp/src/app/components/ds-stoppis-detail/ds-stoppis-detail.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DsStoppisService } from '../service/ds-stoppis-service/ds-stoppis.service';
+import { Stoppis } from '../interface/stoppis';
 @Component({
   selector: 'app-ds-stoppis-detail',
   templateUrl: './ds-stoppis-detail.component.html',
   styleUrls: ['./ds-stoppis-detail.component.css']
 })
 export class DsStoppisDetailComponent implements OnInit {
-
+  kil!: Stoppis;
   constructor(
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private stoppisService: DsStoppisService
+    ) { }
 
   ngOnInit(): void {
     this.getKil()
   }
+
   goBack(): void {
     this.location.back();
   }
-  // TODO lägg till service för att häta specifik kil
+
   getKil(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    console.log(id)
-  }
+    this.stoppisService.getKil(id)
+    .subscribe(kil => {
+      this.kil = kil;
+    });
+  } 
 }
