006ab9a692f01f404d2e9971f10e8d27ee511c3f
Jimmie Van Eijsden <jimmie.van.eijsden@gmail.com>
2024-01-21 12:50:29 +0100

    angular & material 15

--- dorrstopp/src/app/components/ds-stoppis-table/ds-stoppis-table.component.ts
+++ dorrstopp/src/app/components/ds-stoppis-table/ds-stoppis-table.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component,OnInit, SystemJsNgModuleLoader, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { Router } from '@angular/router';
 
 
